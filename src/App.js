569bda5cfa86d2418cb5a629b5b11188f3818767
V-Jgenti-exceed <v.jgenti.exeedteam@gmail.com>
2021-02-12 17:48:59 +0400

    feature: login component basic construction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <Switch>{/* <Route exact path='/login' component={Login} /> */}</Switch>
+      <Switch>
+        <Route path="/login" component={Login} />
+      </Switch>
     </div>
   );
 };
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Login = () => {
+  return (
+    <div className="Login">
+      <h1>LOGIN</h1>
+    </div>
+  );
+};
+
+export default Login;
