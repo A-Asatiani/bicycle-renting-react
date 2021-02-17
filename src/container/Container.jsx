aaad88c639ec38090c8c4186472337d541ab4f2f
V-Jgenti-exceed <v.jgenti.exeedteam@gmail.com>
2021-02-17 15:43:50 +0400

    feature: container component basic css stylizing

--- src/container/Container.jsx
+++ src/container/Container.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './index.css';
 
 const Container = () => {
   return (
-    <div>
+    <div className='container-wrapper'>
       <h1>Container</h1>
     </div>
   );
